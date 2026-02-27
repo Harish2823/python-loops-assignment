8eda01bad2f42706a5dbddcbec27b97d7c8d34fa
Harish2823 <gudeharish6@gmail.com>
2026-02-27 17:45:06 +0530

    deleting un-necessary file

--- git.py
+++
@@ -1,8 +0,0 @@
-player_name = input("Enter player's name: ")
-games_played = int(input("Enter number of games played: "))
-score = int(input("Enter player's score: "))
-Average_score = score / games_played
-print("Player:", player_name)
-print("Games Played:", games_played)
-print("Score:", score)
-print("Average Score:", Average_score)
